2c8c696dcb5d0c93fe9d15b74fa60e5adf25c5f3
qodlsrud20712 <qodlsrud20712@gmail.com>
2019-11-21 10:20:10 +0900

    input 4번 출력

--- chapter_06/input_study.py
+++ chapter_06/input_study.py
@@ -1,8 +1,9 @@
 c = input('정사각형 한 변 길이는?:')
 area = float(c) ** 2
+rec_round = c*4
 
 print("정사각형 넓이: {}".format(area))
-
+print('정사각형 둘레: {}'.format(rec_round))
 a,b = map(int, input().split())
 
 print(a)

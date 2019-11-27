d43ba5bcf48ff2bc4afe95e12fb1b05011002a55
qodlsrud20712 <qodlsrud20712@gmail.com>
2019-11-27 10:19:46 +0900

    11.27 dictionary 과제 while문으로 수정

--- chapter_09/11.26_example.py
+++ chapter_09/11.26_example.py
@@ -2,33 +2,35 @@ file = 'coffeeShopSales.txt'
 
 with open(file, 'r')as f:
     dict1, dict2, dict3, dict4 = {}, {}, {}, {}
-    all_dict, espresso, americano, cafelatte, cappucino, keys, dict_res = [], [], [], [], [], [], []
+    all_dict, espresso, americano, cafelatte, cappucino, keys, dict_res, coffee_menu = [], [], [], [], [], [], [], []
+    cnt, coffee = 0, 0
+    coffee_menu_list = [espresso, americano, cafelatte, cappucino]
 
     header = f.readlines()
     [dict_res.append(header[i].split()) for i in range(len(header))]
-    # for i in range(len(header)):
-    #     dict_res.append(header[i].split())
 
     for i in range(len(header)):
         dict1.update({dict_res[0][i]: dict_res[1][i]})
         dict2.update({dict_res[0][i]:dict_res[2][i]})
         dict3.update({dict_res[0][i]: dict_res[3][i]})
         dict4.update({dict_res[0][i]: dict_res[4][i]})
+
     all_dict = [dict1, dict2, dict3, dict4]
 
     [print(all_dict[i]) for i in range(len(all_dict))]
-    # for i in range(len(all_dict)):
-    #     print(all_dict[i])
 
-    for i in range(len(all_dict)):
-        espresso.append(int(all_dict[i]['에스프레소']))
-        americano.append(int(all_dict[i]['아메리카노']))
-        cafelatte.append(int(all_dict[i]['카페라테']))
-        cappucino.append(int(all_dict[i]['카푸치노']))
+    coffee_menu = list(all_dict[0].keys())
+
+    while 1:
+        if coffee == len(coffee_menu_list):
+            break
+        coffee_menu_list[coffee].append(int(all_dict[cnt][coffee_menu[coffee+1]]))
+        cnt += 1
+        if cnt >= len(all_dict):
+            coffee += 1
+            cnt = 0
 
     [keys.append(key) for key, value in all_dict[0].items()]
-    # for key, value in all_dict[0].items():
-    #     keys.append(key)
 
     total_sum = [sum(espresso), sum(americano), sum(cafelatte), sum(cappucino)]
     total_mean = [sum(espresso) / len(espresso), sum(americano) / len(americano),
